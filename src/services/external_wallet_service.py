cb1767a3f9090f3253c17e559f6cb49b768185db
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2026-04-20 09:37:45 -0400

    feat(transactions): add external wallet mock service for validation

---
+++ src/services/external_wallet_service.py
@@ -0,0 +1,17 @@
+import logging
+
+# --- Inicializacion ---
+
+logger = logging.getLogger(__name__)
+
+# --- Lógica de Servicio Externo ---
+
+class ExternalWalletService:
+    def __init__(self, base_url:str = "http://modulo1-usuario:8000/api"):
+        self.base_url = base_url
+
+    def check_wallet_exist(self, address: str) -> bool:
+        if not address or address.startwith("invalid"):
+            return False
+        
+        return True
